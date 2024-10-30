d7a4ce648a0b0e9969eb53cb4f75e2124c27d59d
jiwoochris <travelandi01@gmail.com>
2024-10-30 10:50:21 +0900

    modify: comment DTO

--- myboard/serializers/comment_serializer.py
+++ myboard/serializers/comment_serializer.py
@@ -3,6 +3,7 @@ from rest_framework import serializers
 from myboard.models import Comment
 
 class CommentSerializer(serializers.ModelSerializer):
+    writer_name = serializers.CharField(source='writer.username', read_only=True)
     class Meta:
         model = Comment
-        fields = "__all__"
+        fields = ["id", "writer_name", "write_date", "content"]
