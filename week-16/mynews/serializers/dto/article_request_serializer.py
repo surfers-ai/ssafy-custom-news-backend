29242477b9d6b61e92976156494543dd4a503bca
jiwoochris <travelandi01@gmail.com>
2025-03-16 21:48:27 +0900

    Remove week-16 project files and configurations, including environment variables, Git ignore, Django settings, models, views, serializers, and other related files.

--- week-16/mynews/serializers/dto/article_request_serializer.py
+++
@@ -1,6 +0,0 @@
-from rest_framework import serializers
-
-
-class ArticleRequestSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    question = serializers.CharField()
--- week-18/mynews/serializers/dto/article_like_request_serializer.py
+++
@@ -1,5 +0,0 @@
-from rest_framework import serializers
-
-
-class ArticleLikeSerializer(serializers.Serializer):
-    article_id = serializers.IntegerField()
